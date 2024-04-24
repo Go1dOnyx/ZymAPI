22c0f5cfe22b198b6ab4c73e5c652036295432ba
Jesus Diaz <jdiazndca21@gmail.com>
2024-04-24 18:52:34 -0500

    Added UserModel

---
+++ ZymAPI/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ZymAPI.Models
+{
+    public class UserModel
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password_Hash { get; set; } = string.Empty;
+        public string First_Name { get; set; } = string.Empty;
+        public string Middle_Name { get; set; } = string.Empty;
+        public string Last_Name { get; set; } = string.Empty;
+        public string Telephone { get; set; } = string.Empty;
+        public string Profile_Url { get; set; } = string.Empty;
+    }
+}
