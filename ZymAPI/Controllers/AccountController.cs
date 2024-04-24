d314a3f79d9712e50cccab315f7764c1bb1fbfb2
Jesus Diaz <jdiazndca21@gmail.com>
2024-04-24 18:01:30 -0500

    Created the account controller for my ZymAPI and added method to implement CRUD operations.

---
+++ ZymAPI/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZymAPI.Controllers
+{
+    public class AccountController : Controller
+    {
+        //Create new user
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        //Get user information 
+        public IActionResult GetUser() 
+        {
+            return View();
+        }
+
+        //Get all users
+        public List<IActionResult> GetAllUsers() 
+        {
+            return View();
+        }
+
+        //Update user info
+        public IActionResult UpdateUser() 
+        {
+            return View();
+        }
+
+        //Delete existing user
+        public IActionResult DeleteUser() 
+        {
+            return View();
+        }
+
+    }
+}
